3f97ecdab1780da09612e55e56475644c5c57671
Mzrt1756 <98002264+Mzrt1756@users.noreply.github.com>
2023-01-25 10:49:36 -0300

    Desafio Follow Camera

--- Assets/Scripts/Clase 7/CameraController.cs
+++ Assets/Scripts/Clase 7/CameraController.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private Transform characterToFollow;
+    [SerializeField] private CinemachineVirtualCamera firstCamera;
+    [SerializeField] private CinemachineVirtualCamera secondCamera;
+    [SerializeField] private CinemachineVirtualCamera thirdCamera;
+    [SerializeField] private CinemachineVirtualCamera shoulderCamera;
+    [SerializeField] private CinemachineVirtualCamera thirdPersonCamera;
+    /*[SerializeField] private Transform characterToFollow;
     [SerializeField] private float speed;
-    [SerializeField] private Vector3 _offset;
+    [SerializeField] private Vector3 _offset;*/
     // Start is called before the first frame update
 
     private void Awake()
     {
-        _offset = transform.position;
+        /*_offset = transform.position;*/
     }
     void Start()
     {
@@ -21,11 +27,42 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            TurnOnCamera(firstCamera, secondCamera, thirdCamera, shoulderCamera, thirdPersonCamera);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            TurnOnCamera(secondCamera, firstCamera, thirdCamera, shoulderCamera, thirdPersonCamera);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            TurnOnCamera(thirdCamera, firstCamera, secondCamera, shoulderCamera, thirdPersonCamera);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            TurnOnCamera(shoulderCamera, firstCamera, secondCamera, thirdCamera, thirdPersonCamera);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad5))
+        {
+            TurnOnCamera(thirdPersonCamera, firstCamera, secondCamera, thirdCamera, shoulderCamera);
+        }
     }
 
-    private void LateUpdate()
+    /*private void LateUpdate()
     {
         transform.position = characterToFollow.position + _offset;
+    }*/
+    private void TurnOnCamera(CinemachineVirtualCamera camToTurnOn, CinemachineVirtualCamera otherCamera1, CinemachineVirtualCamera otherCamera2, CinemachineVirtualCamera otherCamera3, CinemachineVirtualCamera otherCamera4)
+    {
+        //Opcion 1: Apagar y prender el GO
+        camToTurnOn.gameObject.SetActive(true);
+        otherCamera1.gameObject.SetActive(false);
+        otherCamera2.gameObject.SetActive(false);
+        otherCamera3.gameObject.SetActive(false);
+        otherCamera4.gameObject.SetActive(false);
+
+        //Opcion 2: Apagar y prender el componente;
+
     }
 }
---
+++ Assets/Scripts/Desafio 5/CharDesafioCamaras.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharDesafioCamaras : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Transform shootingPoint;
+    [SerializeField] private BulletDesafio5 bulletDesafio5;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+        var direction = new Vector3(x: horizontal, y: 0, z: vertical);
+        Move(direction);
+        var isShooting = Input.GetKeyDown(KeyCode.Mouse0);
+        if (isShooting)
+        {
+            Shoot();
+        }
+
+    }
+    private void Shoot()
+    {
+        Instantiate(bulletDesafio5, shootingPoint.position, shootingPoint.rotation);
+    }
+    private void Move(Vector3 moveDirection)
+    {
+        transform.position += moveDirection * speed * Time.deltaTime;
+    }
+
+
+}
